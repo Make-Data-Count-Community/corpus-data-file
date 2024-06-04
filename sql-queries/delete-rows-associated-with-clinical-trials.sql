a10e2b775569616a13461d4c7d461bf3bca7fb23
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-06-04 12:43:38 +0200

    Add query to delete clinical data from assertions

--- sql-queries/delete-rows-associated-with-clinical-trials.sql
+++ sql-queries/delete-rows-associated-with-clinical-trials.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- Delete non-citation relation types from assertions
+-- Delete clinical trials registries from assertions
 DELETE FROM public.assertions
 WHERE repository_id not in ('fef75a3c-6e48-4170-be9d-415601efb689', '2638e611-ff6f-49db-9b3e-702ecd16176b');
 
