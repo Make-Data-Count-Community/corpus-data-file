f1984e4f929a7efc09f03ce1907673e2eaf874d4
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-06-05 15:57:28 +0200

    Update query to delete assertion withsource 'datacite'

--- sql-queries/delete-non-citation-relation-types.sql
+++ sql-queries/delete-non-citation-relation-types.sql
@@ -2,8 +2,16 @@ BEGIN;
 
 -- Delete non-citation relation types from assertions
 DELETE FROM public.assertions
-WHERE (relation_type_id is not null
-AND relation_type_id not in ('cites', 'is-cited-by', 'references', 'is-referenced-by', 'is-supplemented-by', 'is-supplement-to'));
+WHERE relation_type_id is not null
+    AND source_id = '3644e65a-1696-4cdf-9868-64e7539598d2'
+    AND relation_type_id not in (
+        'cites',
+        'is-cited-by',
+        'references',
+        'is-referenced-by',
+        'is-supplemented-by',
+        'is-supplement-to'
+    );
 
 -- Cascade delete from assertions_affiliations
 DELETE FROM public.assertions_affiliations
