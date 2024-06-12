a7cc31add8242e197a5d7d12d88dd640e6eb3de9
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2024-06-12 17:44:49 +0200

    Update query to remove `not`

--- sql-queries/delete-rows-associated-with-clinical-trials.sql
+++ sql-queries/delete-rows-associated-with-clinical-trials.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 -- Delete clinical trials registries from assertions
 DELETE FROM public.assertions
-WHERE repository_id not in ('fef75a3c-6e48-4170-be9d-415601efb689', '2638e611-ff6f-49db-9b3e-702ecd16176b');
+WHERE repository_id in ('fef75a3c-6e48-4170-be9d-415601efb689', '2638e611-ff6f-49db-9b3e-702ecd16176b');
 
 -- Cascade delete from assertions_affiliations
 DELETE FROM public.assertions_affiliations
