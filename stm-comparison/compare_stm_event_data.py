faede2753485ab3b80150b36964f2d387a931238
lizkrznarich <liz.krznarich@datacite.org>
2024-10-18 11:28:16 -0500

    udpate stm comparison script

--- stm-comparison/compare_stm_event_data.py
+++ stm-comparison/compare_stm_event_data.py
@@ -12,15 +12,30 @@ CITATION_REL_TYPES = ['is-referenced-by',
         'is-supplemented-by'
     ]
 
-SUBJ = 'subj_id'
-OBJ = 'obj_id'
+SUBJ = 'subj-id'
+OBJ = 'obj-id'
 
 def check_if_dois_same(stm_cited_dois, datacite_cited_dois):
     if len(stm_cited_dois) != len(datacite_cited_dois):
         return False
     else:
-        stm_not_in_datacite = [i for i in stm_cited_dois if i not in datacite_cited_dois]
-        datacite_not_in_stm = [i for i in datacite_cited_dois if i not in stm_cited_dois]
+        stm_not_in_datacite = []
+        for s in stm_cited_dois:
+            in_datacite = False
+            for d in datacite_cited_dois:
+                if s in d.strip('https://doi.org/'):
+                    in_datacite = True
+            if in_datacite == False:
+                stm_not_in_datacite.append(s)
+
+        datacite_not_in_stm = []
+        for d in datacite_cited_dois:
+            in_stm = False
+            for s in stm_cited_dois:
+                if d.strip('https://doi.org/') in s:
+                    in_stm = True
+            if in_stm == False:
+                datacite_not_in_stm.append(s)
         if stm_not_in_datacite or datacite_not_in_stm:
             return False
         return True
@@ -44,18 +59,20 @@ def parse_csv(input_file):
             article_doi = row['Article DOI']
             stm_cited_dois = row['Datacite References'].strip('[]').split(',')
             stm_cited_dois = [i.strip('\" ') for i in stm_cited_dois]
+            stm_cited_dois  = [i for i in stm_cited_dois if i != '']
             datacite_cited_dois_subj = get_citation_events(article_doi, SUBJ)
             datacite_cited_dois_obj = get_citation_events(article_doi, OBJ)
             datacite_cited_dois = datacite_cited_dois_subj + [i for i in datacite_cited_dois_obj if i not in datacite_cited_dois_subj]
             stm_same_as_datacite = check_if_dois_same(stm_cited_dois, datacite_cited_dois)
             stm_dois_not_in_datacite = [i for i in stm_cited_dois if i not in datacite_cited_dois]
             datacite_dois_not_in_stm = [i for i in datacite_cited_dois if i not in stm_cited_dois]
-            output.append([article_doi, stm_cited_dois, len(stm_cited_dois), datacite_cited_dois, len(datacite_cited_dois), str(stm_same_as_datacite), len(stm_cited_dois)-len(datacite_cited_dois)])
+            output.append([article_doi, ','.join(stm_cited_dois), len(stm_cited_dois), ','.join(datacite_cited_dois), len(datacite_cited_dois), str(stm_same_as_datacite), len(stm_cited_dois)-len(datacite_cited_dois)])
             #print(f"{article_doi}|{','.join(stm_cited_dois)}|{len(stm_cited_dois)}|{','.join(datacite_cited_dois)}|{len(datacite_cited_dois)}|{str(stm_same_as_datacite)}|{len(stm_cited_dois)-len(datacite_cited_dois)}")
+        return output
 
 def write_to_csv(report_data):
     today = datetime.today().strftime('%Y-%m-%d')
-    with open(today + 'stm_event_data_comparison.csv', 'w') as f:
+    with open(today + '-stm_event_data_comparison.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(['Article DOI', 'STM cited DOIs', 'Count', 'DataCite Event Data cited DOIs', 'Count', 'STM results same as DataCite Event Date', 'Difference in STM vs DataCite cited DOIs count'])
         writer.writerows(report_data)
