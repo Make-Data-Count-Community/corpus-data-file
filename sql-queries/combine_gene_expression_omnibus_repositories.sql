c6a5b14b72af5bffd88aa2d5dac449ada8144d71
kaysiz <ksiziva@gmail.com>
2025-07-17 11:08:56 +0200

    Update query to delete the unused repository

--- sql-queries/combine_gene_expression_omnibus_repositories.sql
+++ sql-queries/combine_gene_expression_omnibus_repositories.sql
@@ -1,5 +1,7 @@
 BEGIN;
 	UPDATE assertions 
 		SET repository_id = 'b2a4aa2b-db3f-456a-8e2b-7d935343385e' -- Gene Expression Omnibus (GEO) - Old/Existing
-		WHERE repository_id = '80a1d446-267a-4468-a474-b69a6242a2b7'; -- Gene Expression Omnibus - New
+		WHERE repository_id = '80a1d446-267a-4468-a474-b69a6242a2b7'; -- Gene Expression Omnibus - new
+
+	DELETE FROM repositories WHERE id = '80a1d446-267a-4468-a474-b69a6242a2b7';
 COMMIT;
