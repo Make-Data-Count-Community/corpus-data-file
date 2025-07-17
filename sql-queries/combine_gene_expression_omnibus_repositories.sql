05533782a3c2181233eca581cc22d9df3a613f2b
kaysiz <ksiziva@gmail.com>
2025-07-17 11:05:46 +0200

    Add query to combine 2 similar repositories

---
+++ sql-queries/combine_gene_expression_omnibus_repositories.sql
@@ -0,0 +1,5 @@
+BEGIN;
+	UPDATE assertions 
+		SET repository_id = 'b2a4aa2b-db3f-456a-8e2b-7d935343385e' -- Gene Expression Omnibus (GEO) - Old/Existing
+		WHERE repository_id = '80a1d446-267a-4468-a474-b69a6242a2b7'; -- Gene Expression Omnibus - New
+COMMIT;
